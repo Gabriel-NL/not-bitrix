34bda15eb6c12ebbbe2be8febdc2577f594d78e5
Gabriel Nogueira Lages <78828285+Gabriel-NL@users.noreply.github.com>
2022-07-17 16:56:34 -0300

    primeiro

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-import express from 'express'
-
+const express = require("express")
 const app = express()
 
 
-app.get('',(request,response)=>{
-    return 
+app.get('/',(request,response)=>{
+    response.sendFile(__dirname + '/src/index.html')
 })
 
 
 app.listen('3333',()=>{
-    console.log('Back end iniciado')
+    console.log('Back end iniciado em http://localhost:3333')
 })
