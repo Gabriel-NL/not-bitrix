5a95cd935ba44fba107ebb196e8202d5e23da6f7
Gabriel Nogueira Lages <78828285+Gabriel-NL@users.noreply.github.com>
2022-07-17 17:21:01 -0300

    Adicionando respostas

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ app.get('/',(request,response)=>{
     response.sendFile(__dirname + '/src/index.html')
 })
 
+app.get('/testeRespostas',(request,response)=>{
+    return response.json(['usuario 1','usuario 2'])
+})
+
+app.post('/testeRespostas',(request, response)=>{
+    return response.json({message: 'criando usuario'})
+})
+app.put('/testeRespostas',(request, response)=>{
+    return response.json({message: 'editando usuario'})
+})
+app.delete('/testeRespostas',(request, response)=>{
+    return response.json({message: 'apagando usuario'})
+})
+
+
+
 
 app.listen(process.env.PORT || '5500',()=>{
     console.log('Back end iniciado em http://localhost:5500')
